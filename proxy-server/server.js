614d8b910fffb91f51f18d64474570d54de7d0a3
Designfavor <DesginFavor>
2024-12-15 06:45:04 -0800

    update

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors({ origin: "https://pfannenstielhof-weingut.webflow.io" }));
+app.use(cors({ origin: "https://pfannenstielhof-weingut.webflow.io/wein-einzel" }));
 
 app.use("/", (req, res) => {
   const url = `https://black-blue-logo-bottle.vercel.app${req.path}`;
