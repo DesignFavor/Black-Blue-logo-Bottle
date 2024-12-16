fbaf85f58e645a53c6c9c8c00c9e1c15ed43063c
Designfavor <DesginFavor>
2024-12-15 22:21:35 -0800

    update

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -1,12 +1,15 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const axios = require('axios');
 const app = express();
 
-app.use(cors({ origin: "https://pfannenstielhof-weingut.webflow.io/wein-einzel" }));
-
-app.use("/", (req, res) => {
-  const url = `https://black-blue-logo-bottle.vercel.app${req.path}`;
-  res.redirect(url);
+app.get('/proxy-script', async (req, res) => {
+  try {
+    const response = await axios.get('https://black-blue-logo-bottle-97eh7cr8s-jawads-projects-ab4a144f.vercel.app/assets/index-Bqfa1y6w.js');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(response.data);
+  } catch (err) {
+    res.status(500).send('Error fetching the file');
+  }
 });
 
-app.listen(3000, () => console.log("Proxy server running on port 3000"));
+app.listen(3000, () => console.log('Proxy running on port 3000'));
